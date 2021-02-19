8206fe1d01b6fdea24280849f8df407e2ce1c0f5
S Reddy <sachitgreddy@email.com>
2021-02-18 20:23:53 -0500

    Step 2 completed

--- code-along/todo.js
+++ code-along/todo.js
@@ -5,67 +5,75 @@
   
 firebase.auth().onAuthStateChanged(async function(user) {
 
-  if (user) {
-    console.log('user logged in')
-  } else {
-    console.log('no user')
-  }
-      
   let db = firebase.firestore()
 
-  document.querySelector('form').addEventListener('submit', async function(event) {
-    event.preventDefault()
+  if (user) {
+    console.log('user logged in')
 
-    let todoText = document.querySelector('#todo').value
+    db.collection('users').doc(user.uid).set({
+      name: user.displayName,
+      email: user.email
+    })
 
-    if (todoText.length > 0) {
-      let docRef = await db.collection('todos').add({
-        text: todoText
-      })
-
-      let todoId = docRef.id
-      console.log(`new todo with ID ${todoId} created`)
-
+    document.querySelector('form').addEventListener('submit', async function(event) {
+      event.preventDefault()
+  
+      let todoText = document.querySelector('#todo').value
+  
+      if (todoText.length > 0) {
+        let docRef = await db.collection('todos').add({
+          text: todoText
+        })
+  
+        let todoId = docRef.id
+        console.log(`new todo with ID ${todoId} created`)
+  
+        document.querySelector('.todos').insertAdjacentHTML('beforeend', `
+          <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
+            <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+            ${todoText}
+          </div>
+        `)
+  
+        document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function(event) {
+          event.preventDefault()
+          document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
+          await db.collection('todos').doc(todoId).delete()
+        })
+        document.querySelector('#todo').value = ''
+      }
+    })
+  
+    let querySnapshot = await db.collection('todos').get()
+    console.log(`Number to todos in collection: ${querySnapshot.size}`)
+  
+    let todos = querySnapshot.docs
+    for (let i=0; i<todos.length; i++) {
+      let todoId = todos[i].id
+      let todo = todos[i].data()
+      let todoText = todo.text
+  
       document.querySelector('.todos').insertAdjacentHTML('beforeend', `
         <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
           <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
           ${todoText}
         </div>
       `)
-
+  
       document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function(event) {
         event.preventDefault()
         document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
         await db.collection('todos').doc(todoId).delete()
       })
-      document.querySelector('#todo').value = ''
     }
-  })
+  
+    
 
-  let querySnapshot = await db.collection('todos').get()
-  console.log(`Number to todos in collection: ${querySnapshot.size}`)
 
-  let todos = querySnapshot.docs
-  for (let i=0; i<todos.length; i++) {
-    let todoId = todos[i].id
-    let todo = todos[i].data()
-    let todoText = todo.text
+  } else {
+    console.log('no user')
 
-    document.querySelector('.todos').insertAdjacentHTML('beforeend', `
-      <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
-        <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
-        ${todoText}
-      </div>
-    `)
-
-    document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function(event) {
-      event.preventDefault()
-      document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
-      await db.collection('todos').doc(todoId).delete()
-    })
-  }
-
-  // Step 1: Un-comment to add FirebaseUI Auth
+    // Step 1: Un-comment to add FirebaseUI Auth
   // // Initializes FirebaseUI Auth
   let ui = new firebaseui.auth.AuthUI(firebase.auth())
 
@@ -79,6 +87,12 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
   // Starts FirebaseUI Auth
   ui.start('.sign-in-or-sign-out', authUIConfig)
+
+  }
+      
+  
+
+  
 })
 
 // Step 3: Hide the form when signed-out
